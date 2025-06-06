492bc2f343345dc29cdcac0e0d3368d33b1c1b21
Varun <varunbha012@gmail.com>
2025-06-06 20:13:03 +0530

    c3

--- frontend/src/components/Room.tsx
+++ frontend/src/components/Room.tsx
@@ -1,18 +1,60 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useSearchParams } from "react-router-dom";
+import { io, type Socket } from "socket.io-client";
 
-
+const URL = "http://localhost:3000";
 
 export const Room = () => {
     const [searchParams, setSearchParams] = useSearchParams();
     const name = searchParams.get('name');
+    const [lobby, setLobby] = useState(true);
+    const [socket, setSocket] = useState<null | Socket>(null);
+
 
     useEffect(() =>{
-        //logic to init user to the room
-    },[name])
-    return (
-        <div >
+        const socket = io(URL, {
+            autoConnect: true,
+        });
+        socket.on('send-offer', ({ roomId }) => {
+            alert("send offer please");
+            socket.emit("offer", {
+                sdp: "sdp", 
+                roomId
+            })
+        });
+
+
+        socket.on('send-offer', ({ roomId, offer }) => {
+            alert("send answer please");
+            setLobby(false);
+            socket.emit("answer", {
+                roomId,
+                sdp: ""
+            })
+        });
+
+        socket.on("answer", ({ roomId, answer }) => {
+            setLobby(false);
+
+            alert("connection done");
+        })
+
+        socket.on("lobby",() => {
+            setLobby(true);
+        })
+
+        setSocket
+    },[name]) 
+
+    if (lobby) {
+        return <div>    
+            Waiting to connect you to someone
+        </div>
+    }
+
+
+    return <div >
         Hi {name}
         </div>
-    );
-    }
+}
+    
